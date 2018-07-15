817ee0bca3c66a791c77dfab2fa25898ffdf106e
beatzz <elsheepo[gmail.com>
2018-07-15 09:27:55 +0900

    Regular updates

--- src/AgeCalculatorFX/Controller.java
+++ src/AgeCalculatorFX/Controller.java
@@ -2,9 +2,12 @@ package AgeCalculatorFX;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -31,7 +34,35 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
 
-    public void clickButton(ActionEvent event) {
+    public void clearTextField(MouseEvent event) {
+        inputTxt.setText("");
+    }
+
+    public void resetOnClick(MouseEvent event) {
+        reset();
+    }
+    public void resetOnReturn(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            reset();
+        }
+    }
+
+    public void calculateOnReturn(KeyEvent e) {
+        if (e.getCode().equals(KeyCode.ENTER)) {
+            calculate();
+        }
+    }
+
+    public void calculateOnClick(MouseEvent e) {
+        calculate();
+    }
+
+    private void reset() {
+        inputTxt.setText("YYYY-MM-DD");
+        resultLbl.setText("");
+    }
+
+    private void calculate() {
         if (isValidDate(inputTxt.getText())) {
             LocalDate dateOfBirth = LocalDate.parse(inputTxt.getText());
             Long age = ChronoUnit.YEARS.between(dateOfBirth, currentDate);
@@ -40,15 +71,6 @@ public class Controller implements Initializable {
             inputTxt.setText("YYYY-MM-DD");
             resultLbl.setText("Please enter your birthday in the specified format.");
         }
-    }
-
-    public void clearTextField(MouseEvent event) {
-        inputTxt.setText("");
-    }
-
-    public void reset(ActionEvent event) {
-        inputTxt.setText("YYYY-MM-DD");
-        resultLbl.setText("");
     }
 
     public void close(ActionEvent event) {
